b540e7def4158d9f4a47adcc081a439cad0444d5
sandor0213 <balogh.sandor0213@gmail.com>
2019-05-21 02:08:20 +0300

    model user + tweet

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   let!(:user){create(:user)}
+
+  subject{
+     build(:tweet, user: user)
+  }
+
+  context "validation" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+    
+    it "should not be valid without body" do
+       subject.body = nil
+       expect(subject).to_not be_valid
+    end
+    
+    it { should validate_presence_of(:body) }
+  end
+
+  context "association" do
+    it { should belong_to(:user) }
+  end
+
+
+ context "when user nickname is already taken" do
+   before do 
+    profile_dub = create(:profile, user: user)
+   end
+   
+   it { should_not be_valid }  
+ end
+ 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{
+  	build(:user)
+  }
+
+
+  describe "validation" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+  end
+
+  describe "associations" do
+    it { should have_many(:tweets).dependent(:destroy) && 
+    	 should have_many(:follower) &&
+    	 should have_many(:following) }
+  end
 end
+
